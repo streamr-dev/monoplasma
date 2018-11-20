b312bf52fb1fe97284255bb31a8db6975628f2d2
Juuso Takalainen <juuso.takalainen@streamr.com>
2018-11-20 18:15:08 +0200

    Add eslint and Travis

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2017
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4,
+            {"SwitchCase": 1}
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "no-console": "warn"
+    }
+}
