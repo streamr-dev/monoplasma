c67d6a7ff4bc054bff4bcbc1df69b2073b6e5c32
Juuso Takalainen <juuso.takalainen@streamr.com>
2019-01-24 13:44:22 +0200

    Revenue sharing demo UI 2nd iteration

--- static_web/js/revenue-sharing-demo-ui.js
+++ static_web/js/revenue-sharing-demo-ui.js
@@ -1,14 +1,16 @@
+/*global document */
+
 const button = document.querySelector("#publish > .round-button")
 button.addEventListener("click", forcePublish)
 
 function forcePublish() {
-	restartAnimation()
+    restartAnimation()
 }
 
 let circle = document.querySelector("#publish > .countdown > .countdown-circle")
 
 function restartAnimation() {
-  const clone = circle.cloneNode(true)
-  circle.parentNode.replaceChild(clone, circle)
-  circle = clone
+    const clone = circle.cloneNode(true)
+    circle.parentNode.replaceChild(clone, circle)
+    circle = clone
 }
