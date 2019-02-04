a4ca83bd58259457203649033b0fc22f7cb40e37
Mariusz Roliński <mariusz@mondoreale.pl>
2019-02-04 12:31:14 +0100

    Tighty up

--- demo/src/containers/Home/index.jsx
+++ demo/src/containers/Home/index.jsx
@@ -1,34 +1,35 @@
 // @flow
 
-/* eslint-disable */
+/* eslint-disable react/no-unused-state */
 
 import React, { Component } from 'react'
 import BN from 'bn.js'
 import HomeComponent from '../../components/Home'
 import Context, { type Props as ContextProps } from '../../contexts/Home'
 
+const { Eth, Web3, ethereum } = window
+
 type State = ContextProps & {
 }
 
 class Home extends Component<{}, State> {
     state = {
-        /* eslint-disable react/no-unused-state */
         account: [
-            ['Total earnings', new BN(0, 2)],
-            ['Earnings frozen', new BN(0, 2)],
-            ['Total withdrawn', new BN(0, 2)],
-            ['Total earnings recorded', new BN(0, 2)],
-            ['Earnings accessible', new BN(0, 2)],
+            ['Total earnings', new BN(0)],
+            ['Earnings frozen', new BN(0)],
+            ['Total withdrawn', new BN(0)],
+            ['Total earnings recorded', new BN(0)],
+            ['Earnings accessible', new BN(0)],
         ],
         revenuePool: [
-            ['Members', new BN(0, 2)],
-            ['Total earnings', new BN(0, 2)],
-            ['Earnings frozen', new BN(0, 2)],
-            ['Contract balance', new BN(0, 2)],
-            ['Total earnings recorded', new BN(0, 2)],
-            ['Earnings available', new BN(0, 2)],
+            ['Members', new BN(0)],
+            ['Total earnings', new BN(0)],
+            ['Earnings frozen', new BN(0)],
+            ['Contract balance', new BN(0)],
+            ['Total earnings recorded', new BN(0)],
+            ['Earnings available', new BN(0)],
             null,
-            ['Total withdrawn', new BN(0, 2)],
+            ['Total withdrawn', new BN(0)],
         ],
         onViewClick: this.onViewClick.bind(this),
         onKickClick: this.onKickClick.bind(this),
@@ -39,29 +40,51 @@ class Home extends Component<{}, State> {
         onStealClick: this.onStealClick.bind(this),
     }
 
+    componentDidMount() {
+        fetch('/data/operator.json').then((/* resp */) => { /* … */ }, console.log.bind(console))
+
+        // From https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
+        if (ethereum) {
+            window.web3 = new Web3(ethereum)
+            window.eth = new Eth(ethereum)
+
+            ethereum.enable().then(() => {
+                // metamaskAddress = ethereum.selectedAddress
+                // document.getElementById("account-found").hidden = !metamaskAddress
+                // document.getElementById("no-accounts").hidden = !!metamaskAddress
+            })
+        } else if (window.web3) {
+            window.web3 = new Web3(window.web3.currentProvider)
+            window.eth = new Eth(window.web3.currentProvider)
+            window.eth.accounts().then((accounts) => {
+                console.log(accounts)
+                // metamaskAddress = accounts[0]
+                // document.getElementById("account-found").hidden = !metamaskAddress
+                // document.getElementById("no-accounts").hidden = !!metamaskAddress
+            })
+        }
+
+        if (!window.eth) {
+            console.log('No Ethereum support detected. Consider installing https://metamask.io/')
+            // document.getElementById("no-metamask").hidden = false
+        }
+    }
+
     onViewClick(address: string) {
-        console.log('View ', address, this)
-        fetch(`/api/members/${address}`).then(resp => resp.json()).then(info => {
-            this.setState({ account: [
-                ['Total earnings', new BN(1, 2)],
-                ['Earnings frozen', new BN(1, 2)],
-                ['Total withdrawn', new BN(1, 2)],
-                ['Total earnings recorded', new BN(2, 2)],
-                ['Earnings accessible', new BN(3, 2)],
-            ]})
-        }).catch(e => {
-            //alert(e)
-            console.error(e)
-            this.setState({ account: [
-                ['Total earnings', new BN("1234.5671234567435456456", 10)],
-                ['Earnings frozen', new BN(1, 2)],
-                ['Total withdrawn', new BN(1, 2)],
-                ['Total earnings recorded', new BN(2, 2)],
-                ['Earnings accessible', new BN("23476238746278334534553454", 10)],
-            ]})
+        fetch(`/api/members/${address}`).then(() => {
+            this.setState({
+                account: [
+                    ['Total earnings', new BN(1)],
+                    ['Earnings frozen', new BN(1)],
+                    ['Total withdrawn', new BN(1)],
+                    ['Total earnings recorded', new BN(2)],
+                    ['Earnings accessible', new BN(3)],
+                ],
+            })
+        }, (error) => {
+            console.log(error)
         })
     }
-    /* eslint-enable react/no-unused-state */
 
     onKickClick(address: string) {
         console.log('Kick', address, this)
@@ -85,40 +108,6 @@ class Home extends Component<{}, State> {
 
     onStealClick() {
         console.log('Steal tokens', this)
-    }
-
-    componentDidMount() {
-        fetch("/data/operator.json")
-        .then(resp => resp.json())
-        .then(json => {
-            config = json
-        }).catch(e => {
-            console.error(e)
-        })
-
-        // From https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
-        if (window.ethereum) {
-            window.web3 = new Web3(ethereum)
-            //window.eth = new window.Eth(ethereum)
-            ethereum.enable().then(function () {
-                //metamaskAddress = ethereum.selectedAddress
-                //document.getElementById("account-found").hidden = !metamaskAddress
-                //document.getElementById("no-accounts").hidden = !!metamaskAddress
-            })
-        } else if (window.web3) {
-            window.web3 = new Web3(web3.currentProvider);
-            //window.eth = new Eth(web3.currentProvider)
-            window.eth.accounts().then(function (accounts) {
-                //metamaskAddress = accounts[0]
-                //document.getElementById("account-found").hidden = !metamaskAddress
-                //document.getElementById("no-accounts").hidden = !!metamaskAddress
-            })
-        }
-
-        if (!window.eth) {
-            console.log("No Ethereum support detected. Consider installing https://metamask.io/")
-            //document.getElementById("no-metamask").hidden = false
-        }
     }
 
     render() {
