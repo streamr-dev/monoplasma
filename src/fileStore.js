e101f98d0b7979de85eec44a3b121d2caacdbd96
Juuso Takalainen <juuso.takalainen@streamr.com>
2019-02-10 20:16:48 +0200

    filestore revamp to support events durability for playback (which zeromq won't provide)
    also tests.

--- src/fileStore.js
+++ src/fileStore.js
@@ -3,64 +3,124 @@ const path = require("path")
 
 const {
     QUIET,
+    MAX_BLOCK_LOG_ENTRY_LENGTH,
 } = process.env
 
 const log = QUIET ? () => {} : console.log
+const maxLogLen = MAX_BLOCK_LOG_ENTRY_LENGTH || 840
 
 /**
  * @typedef {Object} OperatorState
+ * @property {string} tokenAddress Ethereum address of token used by Monoplasma
  */
 /**
- * @param {String} stateStorePath
+ * @param {String} storeDir where json files are stored
  */
-module.exports = (stateStorePath, blockStoreDir) => ({
-    /** @returns {OperatorState} Operator state from the file */
-    loadState: async () => {
-        log(`Loading state from ${stateStorePath}...`)
-        const raw = await fs.readFile(stateStorePath).catch(() => "{}")
-        return JSON.parse(raw)
-    },
+module.exports = (storeDir) => {
+    fs.mkdirSync(storeDir, { recursive: true })
 
-    /**
-     * @param {OperatorState} state Operator state to save
-     */
-    saveState: async state => {
-        const raw = JSON.stringify(state)
-        log(`Saving state to ${stateStorePath}: ${raw.slice(0, 1000)}${raw.length > 1000 ? "... TOTAL LENGTH: " + raw.length : ""}`)
-        const dir = path.dirname(stateStorePath)
-        await fs.mkdir(dir, { recursive: true })
-        return fs.writeFile(stateStorePath, raw)
-    },
+    const stateStorePath = path.join(storeDir, "state.json")
+    const getBlockPath = blockNumber => path.join(storeDir, `block-${blockNumber}.json`)
+    const getEventPath = blockNumber => path.join(storeDir, `events-${blockNumber}.json`)
 
-    /**
-     * @param {number} blockNumber Root-chain block number corresponding to a published side-chain block
-     */
-    loadBlock: async blockNumber => {
-        const treePath = path.join(blockStoreDir, blockNumber + ".json")
-        log(`Loading block ${blockNumber} from ${treePath}...`)
-        const rawString = (await fs.readFile(treePath).catch(() => "[]")).toString()
-        const memberArray = JSON.parse(rawString)
-        return memberArray
-    },
+    return {
+        /** @returns {OperatorState} Operator state from the file */
+        loadState: async () => {
+            log(`Loading state from ${stateStorePath}...`)
+            const raw = await fs.readFile(stateStorePath).catch(() => "{}")
+            return JSON.parse(raw)
+        },
 
-    /**
-     * @param {number} blockNumber Root-chain block number corresponding to a published side-chain block
-     */
-    blockExists: async blockNumber => {
-        await fs.mkdir(blockStoreDir, { recursive: true })
-        const treePath = path.join(blockStoreDir, blockNumber + ".json")
-        return fs.exists(treePath)
-    },
+        /**
+         * @param {OperatorState} state Operator state to save
+         */
+        saveState: async state => {
+            const raw = JSON.stringify(state)
+            log(`Saving state to ${stateStorePath}: ${raw.slice(0, 1000)}${raw.length > 1000 ? "... TOTAL LENGTH: " + raw.length : ""}`)
+            return fs.writeFile(stateStorePath, raw)
+        },
 
-    /**
-     * @param {Array<Object>} members MonoplasmaMember.toObject()s with their earnings etc.
-     * @param {number} blockNumber Root-chain block number corresponding to a published side-chain block
-     */
-    saveBlock: async (members, blockNumber) => {
-        const raw = JSON.stringify(members)
-        const treePath = path.join(blockStoreDir, blockNumber + ".json")
-        log(`Saving block ${blockNumber} to ${treePath}: ${raw.slice(0, 1000)}${raw.length > 1000 ? "... TOTAL LENGTH: " + raw.length : ""}`)
-        await fs.mkdir(blockStoreDir, { recursive: true })
-        return fs.writeFile(treePath, raw)
-    },
-})
+        /**
+         * @param {number} blockNumber Root-chain block number corresponding to a published side-chain block
+         */
+        loadBlock: async blockNumber => {
+            const path = getBlockPath(blockNumber)
+            log(`Loading block ${blockNumber} from ${path}...`)
+            const raw = await fs.readFile(path)
+            const memberArray = JSON.parse(raw)
+            return memberArray
+        },
+
+        /**
+         * @param {number} blockNumber Root-chain block number corresponding to a published side-chain block
+         */
+        blockExists: async blockNumber => {
+            const path = getBlockPath(blockNumber)
+            return fs.exists(path)
+        },
+
+        /**
+         * @typedef {Object} Block Monoplasma side-chain block, see monoplasma.js:storeBlock
+         * @property {number} blockNumber Root-chain block number corresponding to a published side-chain block
+         * @property {Array<Object>} members MonoplasmaMember.toObject()s with their earnings etc.
+         * @property {number} timestamp seconds since epoch, similar to Ethereum block.timestamp
+         * @property {number} totalEarnings sum of members.earnings, to avoid re-calculating it (often needed)
+         */
+        /**
+         * @param {Block} block to be stored, called from monoplasma.js:storeBlock
+         */
+        saveBlock: async (block) => {
+            if (!block || !block.blockNumber) { throw new Error(`Bad block: ${JSON.stringify(block)}`) }
+            const path = getBlockPath(block.blockNumber)
+            const raw = JSON.stringify(block)
+            log(`Saving block ${block.blockNumber} to ${path}: ${raw.length < maxLogLen ? raw : raw.slice(0, maxLogLen) + "... TOTAL LENGTH: " + raw.length}`)
+            if (await fs.exists(path)) { console.error(`Overwriting block ${block.blockNumber}!`) }
+            return fs.writeFile(path, raw)
+        },
+
+        /**
+         * @typedef {Object} Event join/part events are stored in file for playback, others come from Ethereum
+         * @property {number} blockNumber Root-chain block number after which events happened
+         * @property {string} event "Join" or "Part" (TODO: could jsdoc enums handle this?)
+         * @property {Array<string>} addressList
+         */
+        /**
+         * @param {number} blockNumber Root-chain block number after which events happened
+         * @param {Array<Event>} events to be associated with blockNumber
+         */
+        saveEvents: async (blockNumber, events) => {
+            events = Array.isArray(events) ? events : [events]
+            if (events.length < 1) {
+                log(`Empty events given for block #${blockNumber}, not saving`)
+                return
+            }
+            const path = getEventPath(blockNumber)
+            const rawOld = await fs.readFile(path).catch(() => "[]")
+            const oldEvents = JSON.parse(rawOld)
+            log(`Saving ${events.length} event(s like) ${JSON.stringify(events[0])} to ${path} (adding to ${oldEvents.length} events)`)
+            const newEvents = oldEvents.concat(events)
+            const raw = JSON.stringify(newEvents)
+            return fs.writeFile(path, raw)
+        },
+
+        /**
+         * @param {number} fromBlock Sidechain block to load
+         * @param {number} [toBlock=fromBlock+1] load blocks until toBlock, INCLUSIVE (just like getPastEvents)
+         * @returns {Array<Event>} of events, if blocks found, otherwise []
+         */
+        loadEvents: async (fromBlock, toBlock) => {
+            let ret = []
+            const to = toBlock || fromBlock
+            for (let bnum = fromBlock; bnum <= to; bnum++) {
+                const path = getEventPath(bnum)
+                if (await fs.exists(path)) {
+                    log(`Loading events from ${path}`)
+                    const raw = await fs.readFile(path).catch(() => "[]")
+                    const eventList = JSON.parse(raw)
+                    ret = ret.concat(eventList)
+                }
+            }
+            return ret
+        }
+    }
+}
---
+++ test/mocha/fileStore.js
@@ -0,0 +1,52 @@
+/*global describe it */
+const os = require("os")
+const path = require("path")
+const assert = require("assert")
+
+const tmpDir = path.join(os.tmpdir(), `fileStore-test-${+new Date()}`)
+const fileStore = require("../../src/fileStore")(tmpDir)
+
+describe("File system implementation of Monoplasma storage", () => {
+    it("Correctly loads and saves state", async () => {
+        const saved = {testing: "yes"}
+        await fileStore.saveState(saved)
+        const loaded = await fileStore.loadState()
+        assert.deepStrictEqual(saved, loaded)
+    })
+
+    it("Correctly loads and saves blocks", async () => {
+        const saved = {blockNumber: 42, members: [], timeStamp: +new Date(), totalEarnings: "1"}
+        await fileStore.saveBlock(saved)
+        const exists = await fileStore.blockExists(42)
+        assert(exists)
+        const loaded = await fileStore.loadBlock(42)
+        assert.deepStrictEqual(saved, loaded)
+    })
+
+    it("Checks block existence correctly", async () => {
+        const exists = await fileStore.blockExists(68)
+        assert(!exists)
+    })
+
+    it("Correctly loads and saves events", async () => {
+        await fileStore.saveEvents(42, [
+            { blockNumber: 42, event: "Join", addressList: ["0x6dde58bf01e320de32aa69f6daf9ba3c887b4db6"] },
+            { blockNumber: 42, event: "Join", addressList: ["0x47262e0936ec174b7813941ee57695e3cdcd2043"] },
+        ])
+        await fileStore.saveEvents(44, [
+            { blockNumber: 44, event: "Part", addressList: ["0x6dde58bf01e320de32aa69f6daf9ba3c887b4db6"] },
+            { blockNumber: 44, event: "Part", addressList: ["0x47262e0936ec174b7813941ee57695e3cdcd2043"] },
+        ])
+        await fileStore.saveEvents(46, [
+            { blockNumber: 46, event: "Join", addressList: ["0x6dde58bf01e320de32aa69f6daf9ba3c887b4db6"] },
+            { blockNumber: 46, event: "Join", addressList: ["0x47262e0936ec174b7813941ee57695e3cdcd2043"] },
+        ])
+        const loaded = await fileStore.loadEvents(42, 44)
+        assert.deepStrictEqual(loaded, [
+            { blockNumber: 42, event: "Join", addressList: ["0x6dde58bf01e320de32aa69f6daf9ba3c887b4db6"] },
+            { blockNumber: 42, event: "Join", addressList: ["0x47262e0936ec174b7813941ee57695e3cdcd2043"] },
+            { blockNumber: 44, event: "Part", addressList: ["0x6dde58bf01e320de32aa69f6daf9ba3c887b4db6"] },
+            { blockNumber: 44, event: "Part", addressList: ["0x47262e0936ec174b7813941ee57695e3cdcd2043"] },
+        ])
+    })
+})
