b455bdba37b54f24589615b3393653534587c201
Mariusz Roliński <mariusz@mondoreale.pl>
2019-02-14 23:19:23 +0100

    Convert `blockFreezeSeconds` into milliseconds for `sleep`

--- demo/src/containers/Home/index.jsx
+++ demo/src/containers/Home/index.jsx
@@ -272,7 +272,7 @@ class Home extends Component<Props, State> {
             }
             stealInstructions = res
             console.log(`Steal request successful: ${JSON.stringify(stealInstructions)}. Waiting for block to unfreeze...`)
-            return sleep(Number.parseInt(config.blockFreezeSeconds, 10))
+            return sleep(Number.parseInt(config.blockFreezeSeconds || '0', 10) * 1000)
         }).then(() => {
             const { blockNumber, tokens, proof } = stealInstructions
             return monoplasma.withdrawAll(blockNumber, tokens, proof, opts)
