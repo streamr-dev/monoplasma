6eac817b8b68dca7158a79707d5a42adae78b894
Juuso Takalainen <juuso.takalainen@streamr.com>
2020-02-11 12:18:41 +0200

    Added tests to improve coverage (80.56% branches)

--- test/truffle/BalanceVerifier.js
+++ test/truffle/BalanceVerifier.js
@@ -1,7 +1,6 @@
-
 // BalanceVerifier cannot be instantiated so "minimal viable implementation" Airdrop is used instead
 const Airdrop = artifacts.require("./Airdrop.sol")
-const ERC20Mintable = artifacts.require("openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol")
+const DemoToken = artifacts.require("./DemoToken.sol")
 
 const { assertEqual, assertFails } = require("../utils/web3Assert")
 
@@ -21,7 +20,7 @@ contract("BalanceVerifier", accounts => {
     const anotherRecipient = accounts[2]
     const admin = accounts[9]
     before(async () => {
-        token = await ERC20Mintable.new({from: admin, gas: 4000000})
+        token = await DemoToken.new("BalanceVerifier test", "TOK", {from: admin, gas: 4000000})
         airdrop = await Airdrop.new(token.address, {from: admin, gas: 4000000})
         await token.mint(airdrop.address, 1000000, {from: admin})
 
@@ -50,19 +49,23 @@ contract("BalanceVerifier", accounts => {
         it("won't let operator overwrite a root hash (with same block number)", async () => {
             await airdrop.commit(124, "0x1234", "ipfs lol", {from: admin})
             await airdrop.commit(125, "0x2345", "ipfs lol", {from: admin})
-            assertFails(airdrop.commit(125, "0x3456", "ipfs lol", {from: admin}))
+            await assertFails(airdrop.commit(125, "0x3456", "ipfs lol", {from: admin}), "error_overwrite")
+        })
+        it("won't let non-admin commit", async () => {
+            await assertFails(airdrop.commit(128, "0x3456", "ipfs lol", {from: recipient}), "error_notPermitted")
         })
     })
 
     describe("prove & proofIsCorrect & calculateRootHash", () => {
         // see test/merklepath.js
+        let block, member, proof
         it("correctly validate a proof", async () => {
             plasma.addRevenue(1000)
             const memberObj = plasma.getMember(anotherRecipient)
-            const member = MonoplasmaMember.fromObject(memberObj)
+            member = MonoplasmaMember.fromObject(memberObj)
             const root = plasma.tree.getRootHash()
-            const proof = plasma.getProof(anotherRecipient)
-            const block = await publishBlock(root)
+            proof = plasma.getProof(anotherRecipient)
+            block = await publishBlock(root)
             // check that block was published correctly
             assertEqual(block.rootHash, root)
             // check that contract calculates root correctly
@@ -74,6 +77,34 @@ contract("BalanceVerifier", accounts => {
             assertEqual(await token.balanceOf(member.address), 0)
             await airdrop.prove(block.blockNumber, member.address, member.earnings, proof, {from: admin, gas: 4000000})
             assertEqual(await token.balanceOf(member.address), member.earnings)
+        })
+
+        it("fails if you try later with an old (though valid) proof", async () => {
+            await assertFails(airdrop.prove(block.blockNumber, member.address, member.earnings, proof, {from: admin, gas: 4000000}), "error_oldEarnings")
+        })
+
+        it("fails with error_blockNotFound if block is bad", async () => {
+            await assertFails(airdrop.prove(12354678, member.address, member.earnings, proof, {from: admin, gas: 4000000}), "error_blockNotFound")
+        })
+
+        it("fails with error_proof if proof is bad", async () => {
+            await assertFails(airdrop.prove(block.blockNumber, member.address, member.earnings, [], {from: admin, gas: 4000000}), "error_proof")
+        })
+
+        it("fails with error_transfer if contract doesn't have enough tokens", async () => {
+            const badState = new MonoplasmaState(0, [], { saveBlock: () => {} }, admin, 0)
+            badState.addMember(recipient)
+            badState.addMember(anotherRecipient)
+            const tokens = await token.balanceOf(airdrop.address)
+            badState.addRevenue(tokens * 10)
+
+            const memberObj = plasma.getMember(recipient)
+            const member = MonoplasmaMember.fromObject(memberObj)
+            const root = plasma.tree.getRootHash()
+            const proof = plasma.getProof(recipient)
+            const block = await publishBlock(root)
+            assert(await airdrop.proofIsCorrect(block.blockNumber, member.address, member.earnings, proof))
+            await assertFails(airdrop.prove(block.blockNumber, member.address, member.earnings, proof, {from: admin, gas: 4000000}, "error_transfer"))
         })
     })
 })
