6bceeb301c72968ec9086b8dec8b3e0ee6ff6906
Juuso Takalainen <juuso.takalainen@streamr.com>
2020-02-21 13:05:10 +0200

    Enable bytecode optimization
    25k -> 16k bytecode size (in hex characters, which is how it's represented inside the json)

--- truffle-config.js
+++ truffle-config.js
@@ -40,19 +40,19 @@ module.exports = {
    */
 
     networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
-    // development: {
-    //   host: "127.0.0.1",     // Localhost (default: none)
-    //   port: 8545,            // Standard Ethereum port (default: none)
-    //   network_id: "*",       // Any network (default: none)
-    // },
+        // Useful for testing. The `development` name is special - truffle uses it by default
+        // if it's defined here and no other network is specified at the command line.
+        // You should run a client (like ganache-cli, geth or parity) in a separate terminal
+        // tab if you use this network and you must also set the `host`, `port` and `network_id`
+        // options below to some value.
+        //
+        // development: {
+        //   host: "127.0.0.1",     // Localhost (default: none)
+        //   port: 8545,            // Standard Ethereum port (default: none)
+        //   network_id: "*",       // Any network (default: none)
+        // },
 
-    // Another network with more advanced options...
+        // Another network with more advanced options...
         advanced: {
             // port: 8777,             // Custom port
             // network_id: 1342,       // Custom network
@@ -93,15 +93,15 @@ module.exports = {
     // Configure your compilers
     compilers: {
         solc: {
-            version: "0.5.16"    // Fetch exact version from solc-bin (default: truffle's version)
+            version: "0.5.16",      // Fetch exact version from solc-bin (default: truffle's version)
             // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-            // settings: {          // See the solidity docs for advice about optimization and evmVersion
-            //  optimizer: {
-            //    enabled: false,
-            //    runs: 200
-            //  },
-            //  evmVersion: "byzantium"
-            // }
+            settings: {             // See the solidity docs for advice about optimization and evmVersion
+                optimizer: {
+                    enabled: true,
+                    runs: 1500
+                }
+                // evmVersion: "byzantium"
+            }
         }
     }
 }
